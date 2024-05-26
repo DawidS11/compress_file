8b049ec1e51be4a25905c8728bae2425acfa7d38
DawidS11 <dawidsmalcuga@gmail.com>
2024-05-26 12:31:11 +0200

    Add code to do compression

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a simple program to compress files written in Rust. It shows before and after compression weights and the time of compression.
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+extern crate flate2;
+
+use flate2::write::GzEncoder;
+use flate2::Compression;
+use std::env::args;
+use std::fs::File;
+use std::io::copy;
+use std::io::BufReader;
+use std::time::Instant;
+
 fn main() {
-    println!("Hello, world!");
+    // We expect 3 arguments
+    if args().len() != 3 {
+        eprintln!("Usage: `source` `target`");
+        return;
+    }
+    let source_file = File::open(args().nth(1).unwrap());
+    let output_path = args().nth(2).unwrap();
+    let mut input = BufReader::new(source_file.unwrap());
+    let output = File::create(output_path).unwrap();
+
+    let mut encoder = GzEncoder::new(output, Compression::default());
+    let start = Instant::now();
+
+    copy(&mut input, &mut encoder).unwrap();
+    let output = encoder.finish().unwrap();
+    println!(
+        "Source len {:?}",
+        input.get_ref().metadata().unwrap().len()
+    );
+    println!("Target len: {:?}", output.metadata().unwrap().len());
+    println!("Time: {:?}", start.elapsed());
 }
